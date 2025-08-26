f8af44da8fd1f74417541d31fb9255c48ea469ea
Madalin Bogdea <madalinbogdea@icloud.com>
2025-08-26 07:45:31 +0300

    chore: setup firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDe5y36o20UljbDx2ED8g9vO9bU_u1kpOM",
+  authDomain: "blog-app-b4517.firebaseapp.com",
+  projectId: "blog-app-b4517",
+  storageBucket: "blog-app-b4517.firebasestorage.app",
+  messagingSenderId: "554287595961",
+  appId: "1:554287595961:web:bf51dfe736a5db9bdbb668",
+};
+
+const app = initializeApp(firebaseConfig);
