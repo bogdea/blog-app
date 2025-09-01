ab6b941f3bfbcab6ec177144eb379451cad56a31
Madalin Bogdea <madalinbogdea@icloud.com>
2025-09-01 21:43:32 +0300

    style: update individual post page styling

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,8 +1,9 @@
 import { database } from "@/lib/firebase";
 import { get, ref } from "firebase/database";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import type { PostProps } from "@/types/post";
+import { ArrowLeft } from "lucide-react";
 
 const Post = () => {
   const [post, setPost] = useState<PostProps>();
@@ -17,11 +18,18 @@ const Post = () => {
   }, [postId]);
 
   return (
-    <div>
+    <div className="my-5 rounded-2xl border p-5">
+      <Link to={"/blog"} className="mb-5 flex space-x-1">
+        <ArrowLeft /> <span className="font-medium">back to posts</span>
+      </Link>
+
       {post ? (
         <div id={post.id}>
-          <h1>{post.title}</h1>
-          <p>{post.title}</p>
+          <h1 className="mb-2 text-lg font-semibold">{post.title}</h1>
+          <p className="mb-3">{post.content}</p>
+          <span className="text-muted-foreground block text-right text-sm">
+            {new Date(post.createdAt).toLocaleDateString()}
+          </span>
         </div>
       ) : (
         "loading..."
